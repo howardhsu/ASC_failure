de6533fe83c5b0fdefe0ffa9dce5a257c4e3f7bd
Hu Xu <activebus@gmail.com>
2019-11-07 22:24:46 -0500

    update init

--- asclab/__init__.py
+++ asclab/__init__.py
@@ -6,7 +6,5 @@ from .adaweight import AdaWeight
 from .adacontraweight import AdaContraWeight, AdaContraUniWeight
 from .manual_bal import Manual_bal
 from .focalloss import Focalloss
-from .logpweight import LogPWeight
-from .adalogpweight import AdaLogPWeight
 from .predictor import Predictor
 from . import models
